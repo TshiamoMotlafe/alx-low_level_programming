2a86385b3aa7169b8f5c45702c8397b24aa6e27d
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-07-14 05:01:49 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point for the program
+ * This function serves as the entry point for the program execution
+ * It initializes necessary resources, performs the main logic, and
+ * returns the exit status.
+ * Return: The exit status of the program.
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	  printf("%d\n",n);
+	 if (n > 0) {
+        printf("is positive\n");
+    } else if (n == 0) {
+        printf("is zero\n");
+    } else {
+        printf("is negative\n");
+    }
+	return (0);
+}
+ 
