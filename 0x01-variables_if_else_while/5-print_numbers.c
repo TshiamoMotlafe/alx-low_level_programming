a8db9a509e12f108da146d967da5cc1cdc518e3c
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-07-17 19:42:40 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (indicating successful execution)
+ */
+int main(void)
+{
+    int digit;
+
+    for (digit = 0; digit <= 9; digit++)
+    {
+        putchar('0' + digit);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
