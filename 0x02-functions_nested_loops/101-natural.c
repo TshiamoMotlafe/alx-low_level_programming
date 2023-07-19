2886f7c5ad686b43a20a5b3041d28d71a63a5cd5
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-07-18 19:11:03 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,18 +10,18 @@
  */
 int main(void)
 {
-    int limit = 1024;
-    int sum = 0;
-    int i;
+	int limit = 1024;
+	int sum = 0;
+	int i;
 
-    for (i = 1; i < limit; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-            sum += i;
-    }
+	for (i = 1; i < limit; i++)
+	{
+	if (i % 3 == 0 || i % 5 == 0)
+	sum += i;
+	}
 
-    printf("%d\n", sum);
+	printf("%d\n", sum);
 
-    return (0);
+	return (0);
 }
 
