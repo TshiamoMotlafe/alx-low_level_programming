6ededd7ee46efadceede987a1ad5eb5c4cf81601
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-07-18 19:20:08 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,19 +12,19 @@
  */
 int main(void)
 {
-	int num1 = 1, num2 = 2, nextNum, count;
+	unsigned long long num1 = 1, num2 = 2, nextNum, count;
 
-	printf("%d, %d", num1, num2);
+	printf("%llu, %llu", num1, num2);
 
 	for (count = 3; count <= 50; count++)
 	{
 	nextNum = num1 + num2;
-	printf(", %d", nextNum);
+	printf(", %llu", nextNum);
 	num1 = num2;
 	num2 = nextNum;
 	}
 
 	printf("\n");
 
-	return 0;
+	return (0);
 }
