fb7a23fb83465b5410bb1d54cf2f43f3910f35c5
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-07-18 18:52:57 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * print_times_table - Prints the n times table
  * @n: The number of times table to print
@@ -9,26 +8,23 @@
  *              It uses nested loops to iterate over the rows and columns,
  *              and prints each product using the printf function.
  *              The numbers are formatted to align properly.
- *              If n is greater than 15 or less than 0, the function does not print anything.
+ *              If n is greater than 15 or less than 0, does not print anything.
  */
 void print_times_table(int n)
 {
 	int row, column;
-    if (n < 0 || n > 15)
+	if (n < 0 || n > 15)
         return;
 
-    for (row = 0; row <= n; row++)
-    {
+	for (row = 0; row <= n; row++)
+	{
         for (column = 0; column <= n; column++)
-        {
-            int product = row * column;
+	{
+	int product = row * column;
 
-            printf("%d", product);
-
-            if (column != n)
-                printf(", ");
+	printf("%3d", product);
         }
 
         printf("\n");
-    }
+	}
 }
