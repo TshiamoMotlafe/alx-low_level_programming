ffaad3347ce7d9f1786fdaf9e84549d7f95541d0
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-07-21 15:50:32 -0700

    4-print_most_numbes.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int _putchar(char c);
+
+/**
+ * print_most_numbers - Prints the numbers from 0 to 9 (excluding 2 and 4), followed by a new line.
+ */
+void print_most_numbers(void)
+{
+	char digit;
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+	if (digit != '2' && digit != '4')
+	{
+	putchar(digit);
+	}
+	}
+	putchar('\n');
+}
+
