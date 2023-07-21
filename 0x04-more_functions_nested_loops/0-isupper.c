9a5f1538563ef6b1a13ba648ff705875d871a359
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-07-21 14:04:58 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _isupper - Checks if a character is uppercase.
+ *
+ * @c: The character to be checked.
+ *
+ * Return: 1 if the character is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	/* Check if the character is within the ASCII range for uppercase letters */
+	if (c >= 'A' && c <= 'Z')
+	return 1;
+	else
+	return 0;
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a README.md file for the directory 0x04-more functions nested loops
