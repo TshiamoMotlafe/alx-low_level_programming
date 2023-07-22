eba3318184044c3a6af23e34d712eab9386f4325
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-07-21 19:29:12 -0700

    0-main.c, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i = 0;
+    
+    positive_or_negative(i);
+    return (0);
+}
