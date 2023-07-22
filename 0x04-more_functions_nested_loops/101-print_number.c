fdccf2ab74330a0431fc89e61814e3ba5247446c
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-07-21 18:53:03 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * print_number - Prints an integer.
+ *
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
+{
+    if (n < 0)
+    {
+        putchar('-');
+        n = -n;
+    }
+
+    if (n / 10 != 0)
+    {
+        print_number(n / 10);
+    }
+
+    putchar('0' + n % 10);
+}
+
