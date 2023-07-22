d7f18a07d06e0620199a95146c03c95c9c67711f
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-07-21 19:54:57 -0700

    2-largest_number.c, main.h

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,7 +12,8 @@ int main(void)
         printf("Infinite loop incoming :(\n");
         i = 0;
         while (i < 10)
-        { i++;
+        { putchar(i);
+	       	i++;
         }
         printf("Infinite loop avoided! \\o/\n");
 
