54c4603c115c89fe985b089486da810e9486d0fb
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-07-24 15:10:17 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: Pointer to the string whose length needs to be calculated.
+ *
+ * Return: The length of the string (number of characters excluding '\0').
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+	length++;
+	s++;
+	}
+
+	return (length);
+}
+
