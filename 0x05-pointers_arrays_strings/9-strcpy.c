adb15c669f79feb2fb6527841a0e1ecac22cd03d
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-07-24 16:52:49 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * _strcpy - Copies the string pointed to by src to the buffer pointed to by dest.
+ * _strcpy-Copies the string pointed to by src to the buffer pointed to by dest
  * @dest: Pointer to the destination buffer.
  * @src: Pointer to the source string.
  *
@@ -8,7 +8,7 @@
  *
  * Description:
  * This function copies the characters of the null-terminated string pointed to
- * by `src` to the buffer pointed to by `dest`. It also copies the null terminator
+ * by `src` to the buffer pointed to by `dest`. also copies the null terminator
  * '\0' at the end to properly terminate the string in the destination buffer.
  * The function assumes that the destination buffer has enough space to hold
  * the entire source string, including the null terminator.
