3e597178abcc0d0fa7c01e4af72e213b4a0f3f95
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-08-02 18:16:06 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,49 +1,37 @@
-#include<unistd.h>
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * _sqrt_recursion - Calculates the natural square root of a number.
- * @n: The number for which the square root is to be calculated.
+ * _sqrt_recursive - Helper function to calculate the natural square root of a number.
+ * @n: The input number.
+ * @guess: The current guess for the square root.
  *
- * Return: The natural square root of the number.
- *         If n does not have a natural square root, return -1.
+ * Return: The square root if found, otherwise -1.
+ */
+int _sqrt_recursive(int n, int guess)
+{
+	int sqrt = guess * guess;
+
+	if (sqrt == n)
+	{
+	return (guess);
+	}
+	else if (sqrt > n || guess * guess > n)
+	{
+	return (-1);
+	}
+	return (_sqrt_recursive(n, guess + 1));
+}
+/**
+ * _sqrt_recursion - Calculate the natural square root of a number.
+ * @n: The input number.
+ *
+ * Return: The square root if found, otherwise -1.
  */
 int _sqrt_recursion(int n)
 {
-	/* Start searching for the square root from 1 to n */
-	int start = 1;
-
-	int end = n;
-
-	int result = -1;
-
-	int mid;
-
-	/* Base case: If n is 0 or 1, return n */
-	if (n == 0 || n == 1)
+	if (n < 0)
 	{
-	return (n);
+	return (-1);
 	}
-	/* Use binary search to find the square root */
-	while (start <= end)
-	{
-	mid = (start + end) / 2;
-
-	if (mid * mid == n)
-	{
-	result = mid;
-	break;
-	}
-	else if (mid * mid < n)
-	{
-	/* If the square of mid is less than n, search in the right half */
-	start = mid + 1;
-	result = mid;
-	}	
-	else
-	{
-	/* If the square of mid is greater than n, search in the left half */
-	end = mid - 1;
-	}
-	}
-	return (result);
+	return (_sqrt_recursive(n, 1));
 }
