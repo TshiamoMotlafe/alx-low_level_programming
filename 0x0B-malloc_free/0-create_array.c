689e13e47f149ad23387b7420145d0f8e94df42d
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-08-09 02:05:53 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 
 	array = malloc(size * sizeof(char));
 	if (array == NULL)
-        return (NULL);
+	return (NULL);
 
 	for (i = 0; i < size; i++)
 	array[i] = c;
