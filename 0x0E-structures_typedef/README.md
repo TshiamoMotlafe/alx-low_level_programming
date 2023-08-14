1c43b0039ab20657ac860b319e282398a0244825
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-08-14 15:59:05 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is a README.md file for 0x0E. C - Structures, typedef project
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * struct dog - Information about a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: This structure holds information about a dog,
+ * including its name, age, and owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
