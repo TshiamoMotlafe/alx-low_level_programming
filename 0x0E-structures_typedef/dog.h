e333cbf600a7b116ade78636a9de33b7380525ca
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-08-14 16:41:58 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,14 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - Typedef for struct dog
+ *
+ * Description: This typedef provides an alias "dog_t" for the struct dog type.
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 #endif /* DOG_H */
