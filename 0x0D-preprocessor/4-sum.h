5bfd7959c1b33fcb8cb9d2c4ee246bca0450ee1e
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-08-14 17:53:34 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,16 @@
+#ifndef MYMATHMACROS_H
+#define MYMATHMACROS_H
+
+/**
+ * SUM - Computes the sum of two numbers x and y.
+ * @x: The first number.
+ * @y: The second number.
+ *
+ * Description: This macro calculates the sum of two numbers x and y.
+ *
+ * Return: The sum of the input numbers.
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* MYMATHMACROS_H */
+
