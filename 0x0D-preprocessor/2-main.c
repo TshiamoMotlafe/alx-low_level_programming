019e764d26f3fe3be3648b45df4e88eadaf6db6a
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-08-14 17:40:22 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This program prints the name of the file it was compiled from.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+
+	printf("Compiled from: %s\n", __FILE__);
+
+	return (0);
+}
