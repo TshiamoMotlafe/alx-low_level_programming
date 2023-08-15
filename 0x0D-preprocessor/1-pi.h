beeaf6aa1d4979bf5054a495c9b8e999d074bb56
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-08-14 17:33:00 -0700

    1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef MYMATHCONSTANTS_H
+#define MYMATHCONSTANTS_H
+
+/*
+ * Description: This header file defines the PI macro as for 3.14159265359.
+ *              It can be used to replace the value of pi in calculations.
+ */
+
+#define PI 3.14159265359
+
+#endif /* MYMATHCONSTANTS_H */
+
