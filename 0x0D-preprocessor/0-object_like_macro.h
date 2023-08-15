1ae627f43352be0f759e799aae5dea59b38837d2
Tshiamo Motlafe <tsmotlafe@gmail.com>
2023-08-14 17:21:59 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef MYCONSTANTS_H
+#define MYCONSTANTS_H
+
+/* Description: This header file defines the SIZE macro as an abbreviation for 1024. */
+
+#define SIZE 1024
+
+#endif /* MYCONSTANTS_H */
+
